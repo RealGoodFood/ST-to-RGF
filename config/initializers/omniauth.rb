a549d503c5d3904ed7b2335009b39d0eaae60baf
ajey <ajey.potdar@gmail.com>
2013-03-07 13:10:15 +0530

    fb cre changes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ OmniAuth.config.full_host = "http://localhost:3000" #if this remove can see an e
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '426505044096748', 'd0bf3a6cb055626cbc2d4cf38a9e2b7c'
+  provider :facebook, '368106349969719', '73d046490c266d859fc57a78a3502565'
   provider :google_oauth2, '737093073099.apps.googleusercontent.com', 'cbNfqYjoFadGz1QEdsgYBemD', { access_type: "offline", approval_prompt: "" }
   provider :twitter, '2ENJtS2IKIUxFlk2zlPQ', 'mx6AzFCMxWV5iTSvkOFjyfdvdqJWU72a3aZxE3rTW04'
   provider :linkedin, '0i8midv5si3p', 'A9C4M304Z2OuLdnu'
