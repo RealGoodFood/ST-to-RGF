deea38724192d1783233144a3348c26cea001cb9
ajey <ajey.potdar@gmail.com>
2013-04-20 11:42:39 +0530

    worked on open

--- app/controllers/admin/listings_controller.rb
+++ app/controllers/admin/listings_controller.rb
@@ -46,7 +46,7 @@ class Admin::ListingsController < ApplicationController
 
   def destroy
     @listing = Listing.find_by_id(params[:id])
-    @listing.update_attributes(:open, 0 )
+    @listing.update_attribute(:open, 0 )
 
     respond_to do |format|
       format.html { redirect_to admin_listings_path(:type => "listings"), :notice => "Listing successfully closed." }
